2f216319ed021861e48e3ce26acb31227c02bf7b
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-04-14 20:59:18 -0300

    Handling Different Routes

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const express = require('express');
 
 const app = express();
-
-app.use((req, res, next)=> {
-    console.log('in the midleware')
+app.use('/', (req, res, next)=> {
+    console.log('this always runs');
     next();
 });
-app.use((req, res, next)=> {
+app.use('/add-product', (req, res, next)=> {
+    console.log('in the next midleware')
+    res.send('<h1>The "Add product" page</h1>');
+});
+app.use('/', (req, res, next)=> {
     console.log('in the next midleware')
     res.send('<h1>Hello From Express</h1>');
 });
