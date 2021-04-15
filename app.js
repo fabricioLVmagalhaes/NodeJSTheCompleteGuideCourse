38f285e64d14f649292a309d467805eb16b347c3
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-04-15 07:17:20 -0300

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
@@ -11,5 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+});
 
 app.listen(3000);
