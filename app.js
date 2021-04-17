16450664f83928d3d151fa95038b88d9c6683d51
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-04-17 20:47:35 -0300

    Installing & Implementing Pug

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
